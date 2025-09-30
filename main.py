3acafcf4223ee212e057205048afeed0c4f9d793
Reza Karimi <Reza.k.damavandi@gmail.com>
2025-09-30 22:30:32 +0200

    Create main manu structure new update

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 # ---------- Systemutveckling i Python ----------
-
-
-# ----- I detta projekt behöver jag använda: -----
-    # Funktioner, loopar, villkor (if/else), användarinput, listor och dictionaries för att hantera larm
-    # Objektorienterad Programmering (OOP) för att strukturera data "Larm"
-    # JSON för filhantering (spara och ladda konfigurationer/larm till/från JSON-fil)
-    # Använda externa bibliotek (psutil för systeminformation, sendgrid för mejl)
-    # Inbyggda bibliotek (logging, json, datetime, tkinter)
-    # Lambda-funktioner för sortering
-    # Versionshantering med Git & GitHub
+# This is the main file for our monitoring application:
 
 def main_menu():
 
