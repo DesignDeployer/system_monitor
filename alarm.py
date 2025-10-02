aa34bfcda0b4fb8bf8a9d72a9db6a693f37a01e0
Reza Karimi <Reza.k.damavandi@gmail.com>
2025-10-02 18:45:34 +0200

    Add functionality to create new alarms

---
+++ alarm.py
@@ -0,0 +1,11 @@
+# ---------- Systemutveckling i Python ----------
+# This file defines the Alarm Class:
+
+class Alarm:
+    """
+    Represents a single alarm with its type and threshold. 
+    """
+
+    def __init__(self, alarm_type, threshold):
+        self.alarm_type = alarm_type
+        self.threshold = threshold
